6ced0cad5cc55a0ba29aa3aa48ee8203526f83d6
Bern67 <h2o.bern@gmail.com>
2015-02-07 22:40:28 -0800

    minor PCA edits

--- WF_PCA.R
+++ WF_PCA.R
@@ -1,19 +1,31 @@
+#Principal Component Analysis for variables associated with Willamette Falls TDG
 
 dta <-read.csv("TDG.csv")
 dt <-na.omit(dta)
 rm(dta)
 dt<-dt[,-c(1,3,12)]
-boxplot(dt)
-boxplot(scale(dt))
 
 require(dplyr)
-tdg <- sample_frac(dt,0.10,replace=FALSE)
+tdg <- sample_frac(dt,0.075,replace=FALSE)
+detach("package:dplyr", unload=TRUE)
+rm(dt)  # distributions are not normal
+
+boxplot(tdg)
+boxplot(scale(tdg))
+boxplot(scale(log(tdg+1)))
+
+lshap <- lapply(tdg, shapiro.test) #shapiro test on log transformed data
+lres <- sapply(lshap, `[`, c("statistic","p.value"))
+t(lres)
+
+lshap <- lapply(log(tdg+1), shapiro.test) #shapiro test on log transformed data
+lres <- sapply(lshap, `[`, c("statistic","p.value"))
+t(lres)
 
 cor.matrix(tdg) #regression correlatin matrix
 cor.matrix(log((tdg+1)))
 
 cor(tdg)#covariance matrix
-cor()
 
 library(MASS)
 pca <-princomp(scale(log(tdg+1)))
